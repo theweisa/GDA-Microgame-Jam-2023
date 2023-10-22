d9df02f4c5d7276009676e806b6f33d95c858648
ManojSara <msara@ucsc.edu>
2023-10-22 10:50:24 -0700

    Lean Tween fix (????)

--- Assets/Scripts/StartTextTween.cs
+++ Assets/Scripts/StartTextTween.cs
@@ -7,6 +7,7 @@ public class StartTextTween : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Debug.Log("Lean Tween");
         transform.localScale = new Vector3(1.85f*5f, 1.85f*5f, 0f);
         LeanTween.scale(gameObject, new Vector3(1.85f, 1.85f, 0f), 0.2f).setDelay(0.1f);
         LeanTween.alpha(gameObject, 0f, 0.2f).setOnComplete(destroyText).setDelay(1f);
