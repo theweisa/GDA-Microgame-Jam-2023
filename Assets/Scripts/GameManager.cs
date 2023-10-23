5e6dc8f696733767b5237006fe3fe150e580e764
sweetautumndeer <amoulios@yahoo.com>
2023-10-22 22:58:21 -0700

    audio mix and fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -118,7 +118,7 @@ public class GameManager : UnitySingleton<GameManager>
         yield return new WaitForSeconds(1f);
         AudioManager.Instance.PlaySound("WinSound");
 
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(2f);
 
         controller.WinGame();
     }
